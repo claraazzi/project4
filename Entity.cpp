b0f1c9901f5a4c83de6b77c060f1356316d94287
claraazzi <152831913+claraazzi@users.noreply.github.com>
2024-11-15 23:36:03 -0500

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,3 +1,16 @@
+
+/**
+* Author: [Clara EL Azzi]
+* Assignment: Rise of the AI
+* Date due: 2024-11-9, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define FIXED_TIMESTEP 0.0166666f
